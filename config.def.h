2b486df0b0bdc176cbee1acbf79687c13de7466f
Jesse <jesse.frazier.8@gmail.com>
2021-07-07 01:13:45 -0400

    added alt+g as command to launch flameshot to take a screenshot

--- config.def.h
+++ config.def.h
@@ -60,6 +60,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *flameshotcmd[] = {"flameshot", "gui", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -91,6 +92,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_g,      spawn,          {.v = flameshotcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
