45093f5a3a56d490baf2e9affe04554f4bc0077b
JRF8 <jesse.frazier.8@gmail.com>
2022-10-26 03:17:53 -0400

    fix dmenu, re-add some missing commands

--- config.def.h
+++ config.def.h
@@ -119,7 +119,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]    = { "dmenu_run", "-c", "-p", "Run: ", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *scrotclipcmd[] = {"/opt/dwm-scripts/scrotclip.sh", "CLIP", NULL };
 static const char *scrotsavecmd[] = {"/opt/dwm-scripts/scrotclip.sh", "SAVE", NULL };
